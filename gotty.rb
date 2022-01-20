8f378747859f3fc0d08bac34310ca290298c171a
Coding With Hype <67435704+Adal01672@users.noreply.github.com>
2022-01-21 03:45:36 +0600

    Update gotty.rb

--- gotty.rb
+++ gotty.rb
@@ -7,7 +7,7 @@ class Gotty < Formula
   url "https://github.com/yudai/gotty/releases/download/v1.0.1/gotty_darwin_amd64.tar.gz"
   sha256 "ad74832b344480e6647ee9d0c14c783b024735bad8c8a711b1db2284c38cdcfe"
 
-  if OS.linux? && Hardware.is_64_bit?
+   if OS.linux? && Hardware::CPU.is_64_bit?
       url "https://github.com/yudai/gotty/releases/download/v1.0.1/gotty_linux_amd64.tar.gz"
       sha256 "3ff6e0fda6b946e35b0d5fc038054b4b7e8b9558ab697f0a61dc63cebaabd2e1"
   end
